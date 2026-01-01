66f5951d00755a18414800431eccdebb626e5782
Josh Fynly <joshfynly@gmail.com>
2026-01-01 16:06:47 +0100

    Update streamlit_demo.py

--- turnve_app/streamlit_demo.py
+++ turnve_app/streamlit_demo.py
@@ -427,7 +427,7 @@ elif st.session_state.step == "role":
     st.button("← Back", on_click=lambda: st.session_state.update(step="industry"))
 st.header(f"{st.session_state.industry}: Role Selection")
     
-    if st.session_state.industry in FULL_DB:
+   if st.session_state.industry in FULL_DB:
         roles = FULL_DB[st.session_state.industry]["roles"]
 
 for role in roles:
