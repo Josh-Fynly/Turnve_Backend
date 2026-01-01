0ab49de177ce2a66bb0943f53381230fd874c9a0
Josh Fynly <joshfynly@gmail.com>
2026-01-01 14:43:49 +0100

    Update streamlit_demo.py

--- turnve_app/streamlit_demo.py
+++ turnve_app/streamlit_demo.py
@@ -446,7 +446,7 @@ for role in roles:
 st.session_state.current_task_index = 0
                         st.session_state.completed_tasks = []
                         st.rerun()
-    else:
+        else:
         st.error("Error: Industry data missing.")
 
 # =======================
