7e1e0624ed5fe99eba2e4e861a3a38d155e908a7
Josh Fynly <joshfynly@gmail.com>
2026-01-21 06:28:08 +0100

    Update engine.py

--- core_engine/engine.py
+++ core_engine/engine.py
@@ -1,1 +1,9 @@
+from .time import TimeEngine
 
+class SimulationEngine:
+    def __init__(self, session):
+        self.session = session
+        self.time_engine = TimeEngine(session)
+
+    def step(self):
+        self.time_engine.tick()
